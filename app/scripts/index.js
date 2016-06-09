4d910c3d7197e38727f81225e120dbf75349a1f6
Kara Mansel <callmedevmaybe@gmail.com>
2016-06-09 12:33:30 -0400

    Example Ajax

---
+++ app/scripts/index.js
@@ -0,0 +1,32 @@
+var $ = require('jquery');
+var handlebars = require('handlebars');
+
+
+var baseUrl = 'http://swapi.co/api/';
+var planetListItemTemplate = $('#planet-list-item-template').html();
+var template = handlebars.compile(planetListItemTemplate);
+
+$('.js-planets-button').on('click', function(event){
+  event.preventDefault();
+
+  getPlanets();
+});
+
+function getPlanets(){
+  var planetsUrl = baseUrl + 'planets/';
+
+  $.ajax(planetsUrl).done(function(planetList){
+    planetList.results.forEach(function(planet){
+      displayPlanet(planet);
+    });
+  });
+}
+
+function displayPlanet(planet){
+  var html = template(planet);
+  $('.js-planet-list').append(html);
+
+  $.ajax(planet.url).done(function(planetDetails){
+    $('#' + planetDetails.name).append('<span> :: ' + planetDetails.climate + '</span>');
+  });
+}
